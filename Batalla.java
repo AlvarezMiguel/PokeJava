76dfdfa933b7b97c3ca43d16f6d6e8cd33033975
AlavrezMiguel <miguelrolando123@gmail.com>
2019-04-27 07:37:41 -0500

    no queda

--- Batalla.java
+++ Batalla.java
@@ -9,19 +9,19 @@ Pocion arrayPocion[]= new Pocion[3];
 
 public String IniciarJuego(){
 		
-			arrayPokemon1[0]= new Pokemon("Pikachu",100,100,"OK","Electrico",100);
-					arrayPokemon1[1]= new Pokemon("Bulbasaur",100,100,"OK","Hierba",100);
-					arrayPokemon1[2]= new Pokemon("Ivysaur",100,100,"OK","Hierba",100);
-					arrayPokemon1[3]= new Pokemon("Snorlax",100,100,"OK","Normal",100);
-					arrayPokemon1[4]= new Pokemon("Miau",100,100,"OK","Normal",100);
-					arrayPokemon1[5]= new Pokemon("Ratata",100,100,"OK","Normal",100);
+			arrayPokemon1[0]= new Pokemon("Pikachu",250,150,"OK","Electrico",190,200);
+					arrayPokemon1[1]= new Pokemon("Bulbasaur",200,180,"OK","Hierba",100,141);
+					arrayPokemon1[2]= new Pokemon("Ivysaur",160,155,"OK","Hierba",111,152);
+					arrayPokemon1[3]= new Pokemon("Snorlax",240,180,"OK","Normal",100,180);
+					arrayPokemon1[4]= new Pokemon("Miau",100,100,"OK","Normal",100,100);
+					arrayPokemon1[5]= new Pokemon("Ratata",100,100,"OK","Normal",100,100);
 		
-			arrayPokemon2[0]= new Pokemon("Pikachu",100,100,"OK","Electrico",100);
-					arrayPokemon2[1]= new Pokemon("Bulbasaur",100,100,"OK","Hierba",100);
-					arrayPokemon2[2]= new Pokemon("Ivysaur",100,100,"OK","Hierba",100);
-					arrayPokemon2[3]= new Pokemon("Snorlax",100,100,"OK","Normal",100);
-					arrayPokemon2[4]= new Pokemon("Miau",100,100,"OK","Normal",100);
-					arrayPokemon2[5]= new Pokemon("Ratata",100,100,"OK","Normal",100);
+			arrayPokemon2[0]= new Pokemon("Pikachu",100,100,"OK","Electrico",190,100);
+					arrayPokemon2[1]= new Pokemon("Bulbasaur",100,100,"OK","Hierba",100,100);
+					arrayPokemon2[2]= new Pokemon("Ivysaur",100,100,"OK","Hierba",100,100);
+					arrayPokemon2[3]= new Pokemon("Snorlax",100,100,"OK","Normal",100,100);
+					arrayPokemon2[4]= new Pokemon("Miau",100,100,"OK","Normal",100,100);
+					arrayPokemon2[5]= new Pokemon("Ratata",100,100,"OK","Normal",100,100);
 
 		
 			arrayPocion[0]= new Pocion("vida");
@@ -44,20 +44,20 @@ public String IniciarJuego(){
 		
 		
 		System.out.println("Comienza el juego****");
-		System.out.println("elige tu pokemon "+ jugadoruno.getNombre());
+		System.out.println("Elige tu pokemon "+ jugadoruno.getNombre());
 		Integer numPokemon1=entrada.nextInt();
 		System.out.println("Elegiste a "+" "+jugadoruno.pokemones[numPokemon1-1].getApodo());
+		System.out.println("Velocidad :"+jugadoruno.pokemones[numPokemon1-1].getVelocidad());
 		
-		System.out.println("elige tu pokemon "+ jugadordos.getNombre());
+		System.out.println("Elige tu pokemon "+ jugadordos.getNombre());
 		Integer numPokemon2=entrada.nextInt();
-		System.out.println("Elegiste a "+" "+jugadoruno.pokemones[numPokemon2-1].getApodo());
+		System.out.println("Elegiste a "+" "+jugadordos.pokemones[numPokemon2-1].getApodo());
+		System.out.println("Velocidad :"+jugadordos.pokemones[numPokemon2-1].getVelocidad());
 		
+	
+		PrimerTurno(jugadoruno.pokemones[numPokemon1-1],jugadordos.pokemones[numPokemon2-1],jugadoruno,jugadordos);
 
-
-		PrimerTurno(jugadoruno.pokemones[numPokemon1-1],jugadoruno.pokemones[numPokemon2-1]);
-		Turnos(jugadoruno.pokemones[numPokemon1-1],jugadoruno.pokemones[numPokemon2-1]);
-
-
+		//Turnos(jugadoruno.pokemones[numPokemon1-1],jugadoruno.pokemones[numPokemon2-1]);
 
 
 
@@ -65,13 +65,29 @@ public String IniciarJuego(){
 
 		}
 	
-	public  void PrimerTurno (Pokemon pokeuno,Pokemon pokedos){
-		if( pokeuno.getVelocidad()>= pokedos.getVelocidad())
-				System.out.println("Inicia "+pokeuno.getApodo());
-			else
-			System.out.println("Inicia "+pokedos.getApodo());
-	}
+	public  void PrimerTurno (Pokemon pokeuno,Pokemon pokedos, Jugador jugauno, Jugador jugados){
+		if( pokeuno.getVelocidad()>= pokedos.getVelocidad()){ //inicia pokemon 1
+			System.out.println("Inicia :"+pokeuno.getApodo()+ " de "+ jugauno.getNombre());
+			Scanner entrada =new Scanner(System.in);
+			System.out.println("¿Qué realizarás?  1)Usar pocion 2)Ataque 3)Ataque especial");
+			Integer opcion=entrada.nextInt();
+			
+				switch (opcion){
+					case 1: 
+					EleccionPocion(pokeuno,pokedos,jugauno,jugados);
 
+					break;
+					case 2:
+					EleccionAtaque(pokeuno,pokedos,jugauno,jugados);
+					
+					break;
+					case 3:
+					//float multip=EleccionAtaqueEspecial(pokeuno,pokedos,jugauno,jugados);
+
+					}	
+				}
+
+	}
 
 
 	public String Turnos(Pokemon pokeuno,Pokemon pokedos){
@@ -88,11 +104,112 @@ public String IniciarJuego(){
 		}
 
 
-		return "hola";
+		return "turno";
 	}
 
 
 
 
+
+
+	public void EleccionPocion(Pokemon pokeuno,Pokemon pokedos, Jugador jugauno, Jugador jugados){
+		System.out.println("1)De vida 2)De ataque 3)Defensa");
+		Scanner entrada =new Scanner(System.in);
+		Integer opcion=entrada.nextInt();
+							if(opcion==1)
+								if (jugauno.pociones[0].tipoPocion!="*") {	
+								pokeuno.RecibirVidaPocion();
+								jugauno.pociones[0].tipoPocion="*";
+							}else {System.out.println("Ya usaste esa poción");}
+														
+							if(opcion==2)
+								if (jugauno.pociones[1].tipoPocion!="*") {	
+								pokeuno.RecibirAtaquePocion();
+								jugauno.pociones[1].tipoPocion="*";
+							}else {System.out.println("Ya usaste esa poción");}
+							
+
+							if(opcion==3)
+								if (jugauno.pociones[2].tipoPocion!="*") {	
+								pokeuno.RecibirDefensaPocion();
+								jugauno.pociones[2].tipoPocion="*";
+							}else {System.out.println("Ya usaste esa poción");}
 }
 
+
+
+public void EleccionAtaque(Pokemon pokeuno,Pokemon pokedos, Jugador jugauno, Jugador jugados){
+		System.out.println("Atacando ... ...");
+		double multi=MultiplicadorElemental(pokeuno,pokedos);
+		double ataque =((pokeuno.getAtaque()-pokedos.getDefensa())*(multi));
+
+		System.out.println(ataque);
+		System.out.println("La vida de "+ pokedos.getApodo()+" es "+ pokedos.setVida(ataque));
+		
+		
+}
+
+
+
+public double MultiplicadorElemental(Pokemon atacante, Pokemon atacado ){
+		double multiplicador=1;
+		if (atacante.tipo=="Fuego"){
+			//• Fuego: x2 de daño a tipo Hierba, x0.5 a tipo Agua o Fuego, x1 a otro tipo.
+			if(atacado.tipo=="Hierba"){
+				multiplicador=2;
+			}
+			else if ((atacado.tipo=="Agua") || (atacado.tipo=="Fuego"))
+			{
+				multiplicador=.5;
+			}
+			else{
+				multiplicador=1;
+			}
+
+		}
+		else if (atacante.tipo=="Agua"){
+			//• Agua: x2 de daño a tipo Fuego, x0.5 a tipo Agua o Hierba, x1 a otro tipo.
+			if(atacado.tipo=="Fuego"){
+				multiplicador=2;
+			}
+			else if ((atacado.tipo=="Agua") || (atacado.tipo=="Hierba"))
+			{
+				multiplicador=.5;
+			}
+			else{
+				multiplicador=1;
+			}
+
+
+		}
+		else if(atacante.tipo=="Hierba"){
+			//• Hierba: x2 de daño a tipo Agua, x0.5 a tipo Hierba o Fuego, x1 a otro tipo.
+			if(atacado.tipo=="Agua"){
+				multiplicador=2;
+			}
+			else if ((atacado.tipo=="Hierba") || (atacado.tipo=="Fuego"))
+			{
+				multiplicador=.5;
+			}
+			else{
+				multiplicador=1;
+			}
+
+		}
+		else if(atacante.tipo=="Electrico"){
+			//• Eléctrico: x2 de daño a tipo Agua, x0.5 a tipo Hierba o Eléctrico, x1 a otro tipo.
+			if(atacado.tipo=="Agua"){
+				multiplicador=2;
+			}
+			else if ((atacado.tipo=="Hierba") || (atacado.tipo=="Electrico"))
+			{
+				multiplicador=.5;
+			}
+			else{
+				multiplicador=1;
+			}
+
+		}
+		return multiplicador;
+	}
+}
--- Pokemon.java
+++ Pokemon.java
@@ -5,6 +5,7 @@ public class Pokemon {
 	String estado; //(paralizado, quemado, envenenado, etc.).
 	String tipo; //(Agua, Fuego, Hierba y Eléctrico). (4)
 	double velocidad;
+	double ataque;
 
 // GETTERS
 	public String getApodo(){
@@ -26,15 +27,62 @@ public class Pokemon {
 	public double getVelocidad(){
 		return velocidad;
 	}
-/* Fin de los getters*/ 
+	public double getAtaque(){
+		return ataque;
+	}
+
+
+	public double setVida(double ataque){
+		vida=vida-ataque;
+		return vida;	
+	}
 
 /*Método constructor*/
-public Pokemon(String apodo, double vida, double defensa, String estado, String tipo, double velocidad){
+	public Pokemon(String apodo, double vida, double defensa, String estado, String tipo, double velocidad, double ataque){
 	this.apodo= apodo;
 	this.vida= vida;
 	this.defensa= defensa;
 	this.estado=estado;
 	this.tipo=tipo;
 	this.velocidad=velocidad;
-}
+	this.ataque=ataque;
+	}
+
+
+//Métodos Pokemon
+
+// 	public double RecibirDanio(){
+
+
+// 	}
+
+	public void RecibirVidaPocion(){ 
+		vida =vida + vida*.2;
+		
+		System.out.println("La vida de "+apodo +"es de "+ vida);
+ 	}
+
+ 	public void RecibirAtaquePocion(){
+ 		ataque= ataque+ ataque*.1;
+
+ 		System.out.println("El ataque de "+apodo+" es de "+ ataque);
+ 	}
+ 	public void RecibirDefensaPocion(){
+ 		defensa=defensa+ defensa*.1;
+ 		System.out.println("La  defensa de "+apodo+ "es de "+defensa);
+ 	}
+
+// 	public double RealizarAtaque1(){
+
+// 	}
+
+// 	public double RealizarAtaque2(){
+		
+// 	}
+
+
+// public double RecibirAtaque(){
+
+// }
+
 }
